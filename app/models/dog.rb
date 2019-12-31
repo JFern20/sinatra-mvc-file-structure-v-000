34040dc057c4c0ce6b25a40d6e93b88bf3012d10
Jonathan Fernandez <jfern20@gmail.com>
2019-12-31 15:32:53 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+  
+  @all = []
+  
+  def initialize(name, breed, age)
+    @name= name 
+    @breed= breed 
+    @age = age
+  end 
+  
+  def self.all 
+    @all
+  end 
+  
+  
+  
+end 
